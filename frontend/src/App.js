adbd8e612d8f1b5a496d96d9bf08abae406e6dc1
Aleksander Grzybowski <kelog@orange.pl>
2016-10-19 21:48:36 +0200

    [F] Empty React app and Gradle automation

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                React works, if 2 + 2 = { 2 + 2 }
+            </div>
+        );
+    }
+}
+
+export default App;
