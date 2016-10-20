18c904e22a3b374969f75eff2e6d64f02dd527e2
Aleksander Grzybowski <kelog@orange.pl>
2016-10-20 18:11:19 +0200

    Add CORS plugin, test request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import backendUrl from './backendUrl';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            input: '',
+            serverResponse: null
+        }
+    }
+    
+    handleInputChange = (e) => {
+        this.setState({input: e.target.value})
+    };
+    
+    makeRequest = () => {
+        axios.get(`${backendUrl}/test/square?number=${this.state.input}`)
+            .then((response) => this.setState({serverResponse: JSON.stringify(response)}))
+            .catch((error) => this.setState({serverResponse: JSON.stringify(error)}))
+    };
+    
     render() {
         return (
             <div className="App">
-                React works, if 2 + 2 = { 2 + 2 }
+                <input value={this.state.input} onChange={this.handleInputChange}/>
+                <button onClick={this.makeRequest}>Click</button>
+                <span>Response: {this.state.serverResponse}</span>
             </div>
         );
     }
