97bd8c9e2b28f121b03b63a3f975a699f026e9d3
Aleksander Grzybowski <kelog@orange.pl>
2016-10-21 09:32:51 +0200

    [B] Create more test users

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -11,14 +11,17 @@ class BootStrap {
     static void sampleUsers() {
         Role chatUserRole = new Role(authority: 'ROLE_CHAT_USER').save()
 
-        User u1 = new User(username: 'u1', password: 'u1').save()
-        new UserRole(user: u1, role: chatUserRole).save()
-
-        User u2 = new User(username: 'u2', password: 'u2').save()
-        new UserRole(user: u2, role: chatUserRole).save()
+        (0..9).each { int index ->
+            createChatUser('u' + index, chatUserRole)
+        }
 
         Role adminUserRole = new Role(authority: 'ROLE_ADMIN').save()
         User admin = new User(username: 'admin', password: 'p4ssw0rd').save()
         new UserRole(user: admin, role: adminUserRole).save()
+    }
+
+    static void createChatUser(String pattern, Role role) {
+        User user = new User(username: pattern, password: pattern).save()
+        new UserRole(user: user, role: role).save()
     }
 }
