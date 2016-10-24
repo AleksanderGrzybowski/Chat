e7fec8efd6f7c47581c3bbd4671ee0f19e2bff37
Aleksander Grzybowski <kelog@orange.pl>
2016-10-24 20:49:28 +0200

    [F] Reset login state after logout, cleanup

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -1,34 +1,40 @@
-const initialState = {loggedIn: false, loginError: false, username: null, password: null};
-export const login = (state = initialState, action) => {
+const initialStateLogin = {loggedIn: false, loginError: false, username: null, password: null};
+export const login = (state = initialStateLogin, action) => {
     switch (action.type) {
         case 'LOGIN_SUCCESSFULL':
             return {loggedIn: true, loginError: false, username: action.username, password: action.password};
         case 'LOGIN_ERROR':
-            return Object.assign({}, state, initialState, {loginError: true});
+            return Object.assign({}, state, initialStateLogin, {loginError: true});
         case 'LOGOUT':
-            return initialState;
+            return initialStateLogin;
         default:
             return state;
     }
 };
 
-export const usersList = (state = {users: [], currentUserId: null}, action) => {
+const initialStateUsersList = {currentUserId: null, users: []};
+export const usersList = (state = initialStateUsersList, action) => {
     switch (action.type) {
         case 'LOAD_USERS':
             return Object.assign({}, state, {users: action.users});
         case 'CHANGE_SELECTED_USER':
             return Object.assign({}, state, {currentUserId: action.userId});
+        case 'LOGOUT':
+            return initialStateUsersList;
         default:
             return state
     }
 };
 
-export const conversation = (state = {currentUserId: null, messages: []}, action) => {
+const initialStateConversation = {currentUserId: null, messages: []};
+export const conversation = (state = initialStateConversation, action) => {
     switch (action.type) {
         case 'LOAD_CONVERSATION':
             return Object.assign({}, state, {messages: action.messages});
         case 'CHANGE_SELECTED_USER':
             return Object.assign({}, state, {currentUserId: action.userId});
+        case 'LOGOUT':
+            return initialStateConversation;
         default:
             return state
     }
