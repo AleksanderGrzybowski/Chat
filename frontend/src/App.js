12c2a0def5f19d81a9304763b816eb26e5ea7297
Aleksander Grzybowski <kelog@orange.pl>
2016-10-24 13:36:38 +0200

    [F] Logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
                     </Navbar.Brand>
                 </Navbar.Header>
                 <Nav>
-                    <NavItem onClick={this.logout}>Logout</NavItem>
+                    <NavItem onClick={this.props.onLogout}>Logout</NavItem>
                 </Nav>
             </Navbar>
         );
