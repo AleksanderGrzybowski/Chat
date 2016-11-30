3fe83cb8463412d488ca8f6fa0bf3b85eb9cb1a5
Aleksander Grzybowski <kelog@orange.pl>
2016-11-30 15:37:49 +0100

    [B] Fix error handling for java.lang.Error

--- grails-app/controllers/chat/ErrorController.groovy
+++ grails-app/controllers/chat/ErrorController.groovy
@@ -5,7 +5,7 @@ import grails.converters.JSON
 class ErrorController  {
     def index() {
         response.status = 500
-        Exception exception = request.exception.cause.target
+        Throwable exception = request.exception.cause.target
         render([message: exception.message, 'class': exception.class] as JSON)
     }
 }
