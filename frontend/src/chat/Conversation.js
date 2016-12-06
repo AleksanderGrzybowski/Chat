820055e559c7beebd3a27126528284239fe580a3
Aleksander Grzybowski <kelog@orange.pl>
2016-12-06 18:38:55 +0100

    [F] Scroll down the window when necessary

--- frontend/src/chat/Conversation.js
+++ frontend/src/chat/Conversation.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Message from './Message';
 import { ReactInterval } from 'react-interval';
 
 export default class Conversation extends Component {
+    // http://blog.vjeux.com/2013/javascript/scroll-position-with-react.html
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this.refs.scrollable);
+        this.shouldScrollToBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
+    }
+
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this.refs.scrollable);
+            node.scrollTop = node.scrollHeight
+        }
+    }
+
     render() {
         const content = (this.props.messages.length === 0) ? (
             <h3 className="text-center">No messages yet</h3>
@@ -19,7 +33,7 @@ export default class Conversation extends Component {
         );
 
         return (
-            <div style={{height: '70vh', overflowY: 'scroll'}}>
+            <div style={{height: '70vh', overflowY: 'scroll'}} ref="scrollable">
                 {content}
                 <ReactInterval callback={this.props.refreshCurrentConversation} enabled={true} timeout={1000}/>
             </div>
