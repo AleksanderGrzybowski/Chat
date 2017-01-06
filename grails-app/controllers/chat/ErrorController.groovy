3109549f3cc80f162d1bcd00a9685760bdd833fd
Aleksander Grzybowski <kelog@orange.pl>
2017-01-06 11:45:25 +0100

    [B] Workaround for strange exceptions without cause

--- grails-app/controllers/chat/ErrorController.groovy
+++ grails-app/controllers/chat/ErrorController.groovy
@@ -5,7 +5,7 @@ import grails.converters.JSON
 class ErrorController  {
     def index() {
         response.status = 500
-        Throwable exception = request.exception.cause.target
-        render([message: exception.message, 'class': exception.class] as JSON)
+        Throwable exception = request.exception?.cause?.target
+        render([message: exception.message, 'class': exception?.class] as JSON)
     }
 }
