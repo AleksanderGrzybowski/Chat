26183cd5d5a5ce046e70ccaed747ebf319d32990
Aleksander Grzybowski <kelog@orange.pl>
2017-07-08 19:56:31 +0200

    Better error message

--- frontend/src/chat/ErrorPage.js
+++ frontend/src/chat/ErrorPage.js
@@ -3,7 +3,7 @@ import { Alert } from 'react-bootstrap';
 
 const ErrorPage = () => (
     <Alert bsStyle="warning">
-        <h1 className="text-center">Oops, something went wrong</h1>
+        <h1 className="text-center">Oops, server returned an error.</h1>
         <p className="text-center">Please try again later.</p>
     </Alert>
 );
